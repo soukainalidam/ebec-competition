260b9382ae29693abdfdb54bccd6fd986a65411c
Soukaina lidam <soukaina.lidam@student-cs.fr>
2021-04-09 23:05:31 +0000

    Update reverseCoding.py

--- reverseCoding.py
+++ reverseCoding.py
@@ -8,3 +8,13 @@ def reverse_coding(Lat,Lng):
     town=g.town
     street=g.street
     return ({'Lat':Lat,'Lng':Lng,'Ville':town,'Nom de la voie':street,'Début du tronçon':'deb', 'Fin de tronçon':'fin'})
+
+##Return the trees count in an area or a bounding box
+from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
+from OSMPythonTools.nominatim import Nominatim
+nominatim = Nominatim()
+areaId = nominatim.query('Courbevoie, France').areaId()
+overpass = Overpass()
+query = overpassQueryBuilder(area=areaId, elementType='node', selector='"natural"="tree"', out='count')
+result = overpass.query(query)
+result.countElements()
